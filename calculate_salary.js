d86195eeea42c4552943cd00ff0d6e33c930146e
Vpokus <newman3582@gmail.com>
2024-02-14 00:52:51 +0300

    перенос функции calculate_salary в тест

---
+++ calculate_salary.js
@@ -0,0 +1,9 @@
+function calculate_salary(role) {
+  let salary = 0;
+  if (role === 'engineer') {
+    salary = 4000;
+  } else {
+    salary = 1500;
+  }
+  return salary;
+}
--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-let number_of_employees = 10;
-let salary = 2000;
-let total_salary = number_of_employees * salary;
+let worker1 = {
+  name: 'Вася',
+  role: 'worker',
+  age: 23,
+};
+
+let engineer1 = {
+  name: 'Григорий Палыч',
+  role: 'engineer',
+  age: 42,
+};
+
+let worker2 = {
+  name: 'Гриша',
+  role: 'worker',
+  age: 25,
+};
+
+let engineer2 = {
+  name: 'Сан Саныч',
+  role: 'engineer',
+  age: 54,
+};
+
+let employees = [worker1, engineer1, worker2];
+employees.push(engineer2);
+
+let total_salary = 0;
+for (let index = 0; index < employees.length; index = index + 1) {
+  let employee = employees[index];
+  let salary = calculate_salary(employee.role);
+  total_salary = total_salary + salary;
+}
 console.log(total_salary);
