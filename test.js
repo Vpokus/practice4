10a6f7f7bf8303c8ffc6e68a3e02cc3455f8b184
Vpokus <newman3582@gmail.com>
2024-02-15 22:22:56 +0300

    ok

--- test.js
+++ test.js
@@ -5,4 +5,10 @@ let engineer_salary = calculate_salary('engineer');
 if (engineer_salary !== 4000) {
   passed = false;
 }
+
+let worker_salary = calculate_salary('worker');
+if (worker_salary !== 1500) {
+  passed = false;
+}
+
 console.log(passed);
